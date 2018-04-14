e3bd485eb0616f9dc2fdc283b84b261e7f6f09d7
Skylerly <anderskyler@gmail.com>
2018-04-14 19:00:25 -0400

    loss function update
    loss still returning nan -- I think its because of the for loops(need to write them as tf/keras functions instead of regular python)

--- SkyUtils.py
+++ SkyUtils.py
@@ -8,6 +8,7 @@ import cv2
 import numpy as np
 from keras import backend as K
 import keras
+import tensorflow as tf
 
 def drawRectsFromPred(predictions, img):
     for i in range(7):
@@ -27,9 +28,13 @@ def customloss(gt, pred):
     results and L2 loss to confidence and box coordinates"""
     #assert gt.shape == pred.shape
     
+    
+    f1 = lambda: tf.constant(0.5)
+    f2 = lambda: tf.constant(0.5)
+    
     loss = K.variable(0.,dtype='float32')
-    print(gt.shape)
-    print(pred.shape)
+#    print(gt.shape)
+#    print(pred.shape)
     try: 
         batch_size = int(gt.shape[0])
     except:
@@ -38,9 +43,15 @@ def customloss(gt, pred):
     for batch in range(batch_size):
         for i in range(7):
             for j in range(7):
+                 # Check if the box contains an object and adjust loss accordingly
+                scale_factor = tf.case([(tf.less(pred[batch,i,j,0], tf.constant(1.0)), f1)], default=f2)
                 # Catgeorical crossentropy for classwise confidence
-                loss = K.add(loss , K.sum(K.categorical_crossentropy(gt[batch,i,j,5:], pred[batch,i,j,5:])))
+                loss_from_class_preds = K.categorical_crossentropy(gt[batch,i,j,5:], pred[batch,i,j,5:])
+                loss_from_class_preds = tf.multiply(loss_from_class_preds, scale_factor)
+                loss = tf.add(loss, loss_from_class_preds)
                 # MSE for confidence and bounding box predictions
-                loss = K.add(loss , K.mean(K.square(gt[i,j,:5] - pred[i,j,:5])))
+                bb_mse = K.mean(K.square(gt[i,j,:5] - pred[i,j,:5]))
+                bb_mse = tf.multiply(bb_mse, scale_factor)
+                loss = tf.add(loss , bb_mse)
             
-    return loss / (batch_size * 25 * 25)
+    return tf.divide(loss , batch_size)
