6c2f3bdbe5ad0de6c8f89db54c711a107b084809
Skylerly <anderskyler@gmail.com>
2018-04-14 22:52:42 -0400

    color visualization
    Added overlayPredsByColor in SkyUtils to show predictions in a heatmap -- randomly assigns colors to each class right now, will decide on a good map later

--- SkyUtils.py
+++ SkyUtils.py
@@ -9,6 +9,7 @@ import numpy as np
 from keras import backend as K
 import keras
 import tensorflow as tf
+import random
 
 def drawRectsFromPred(predictions, img):
     for i in range(7):
@@ -22,6 +23,23 @@ def drawRectsFromPred(predictions, img):
                 cv2.rectangle(img, (x,y),(x+w,y+h), (0,255,0), 1)
     return img
 
+def overlayPredsByColor(predictions,img):
+    label_colors = {}
+    preds = predictions[:,:,5:]
+    colormap = np.zeros((7,7,3),dtype='uint8')
+    for i in range(20):
+        label_colors[i] = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        
+    for i in range(7):
+        for j in range(7):
+            colormap[i,j,:] = label_colors[np.argmax(preds[i,j,:])]
+            
+    alpha = 0.4
+    colormap = cv2.resize(colormap, (224,224),0,0,0,cv2.INTER_NEAREST)
+    output = img.copy()
+    output = cv2.addWeighted(colormap, alpha, output, 1 - alpha, 0, output)
+    return output
+    
 
 def customloss(gt, pred):
     """ Custom loss function to apply categorical cross entropy to the classification
@@ -32,26 +50,38 @@ def customloss(gt, pred):
     f1 = lambda: tf.constant(0.5)
     f2 = lambda: tf.constant(0.5)
     
+    scale_factor = tf.case([(tf.less(pred[batch,i,j,0], tf.constant(1.0)), f1)], default=f2)
+    # Catgeorical crossentropy for classwise confidence
+    loss_from_class_preds = K.categorical_crossentropy(gt[batch,i,j,5:], pred[batch,i,j,5:])
+    loss_from_class_preds = tf.multiply(loss_from_class_preds, scale_factor)
+    loss = tf.add(loss, loss_from_class_preds)
+    # MSE for confidence and bounding box predictions
+    bb_mse = K.mean(K.square(gt[i,j,:5] - pred[i,j,:5]))
+    bb_mse = tf.multiply(bb_mse, scale_factor)
+    loss = tf.add(loss , bb_mse)
+    
     loss = K.variable(0.,dtype='float32')
 #    print(gt.shape)
 #    print(pred.shape)
-    try: 
-        batch_size = int(gt.shape[0])
-    except:
-        batch_size = 32
+#    try: 
+#        batch_size = int(gt.shape[0])
+#    except:
+#        batch_size = 32
+#        
         
-    for batch in range(batch_size):
-        for i in range(7):
-            for j in range(7):
-                 # Check if the box contains an object and adjust loss accordingly
-                scale_factor = tf.case([(tf.less(pred[batch,i,j,0], tf.constant(1.0)), f1)], default=f2)
-                # Catgeorical crossentropy for classwise confidence
-                loss_from_class_preds = K.categorical_crossentropy(gt[batch,i,j,5:], pred[batch,i,j,5:])
-                loss_from_class_preds = tf.multiply(loss_from_class_preds, scale_factor)
-                loss = tf.add(loss, loss_from_class_preds)
-                # MSE for confidence and bounding box predictions
-                bb_mse = K.mean(K.square(gt[i,j,:5] - pred[i,j,:5]))
-                bb_mse = tf.multiply(bb_mse, scale_factor)
-                loss = tf.add(loss , bb_mse)
+        
+#    for batch in range(batch_size):
+#        for i in range(7):
+#            for j in range(7):
+#                 # Check if the box contains an object and adjust loss accordingly
+#                scale_factor = tf.case([(tf.less(pred[batch,i,j,0], tf.constant(1.0)), f1)], default=f2)
+#                # Catgeorical crossentropy for classwise confidence
+#                loss_from_class_preds = K.categorical_crossentropy(gt[batch,i,j,5:], pred[batch,i,j,5:])
+#                loss_from_class_preds = tf.multiply(loss_from_class_preds, scale_factor)
+#                loss = tf.add(loss, loss_from_class_preds)
+#                # MSE for confidence and bounding box predictions
+#                bb_mse = K.mean(K.square(gt[i,j,:5] - pred[i,j,:5]))
+#                bb_mse = tf.multiply(bb_mse, scale_factor)
+#                loss = tf.add(loss , bb_mse)
             
     return tf.divide(loss , batch_size)
--- model.py
+++ model.py
@@ -24,7 +24,9 @@ image_IDs = os.listdir('D:/VOC_individual_np/images')
 image_IDs = os.listdir('D:/VOCtrainval_11-May-2012/VOCdevkit/VOC2012/JPEGImages/')
 labels = os.listdir('D:/VOC_individual_np/labels')
 conv_base = VGG16(weights='imagenet',include_top=False, input_shape=(224, 224, 3))
-
+for layer in conv_base.layers:
+    layer.trainable = False
+    
 # BB regression
 input = Input(shape=(224,224,3))
 conv_base = VGG16(weights='imagenet',include_top=False, input_shape=(224, 224, 3))(input)
@@ -50,18 +52,18 @@ model = Model(inputs=input, outputs=out)
 #model.add(Dense(7 * 7 * 25))
 #model.add(Reshape((7,7,25)))
 
-for layer in conv_base.layers:
-    layer.trainable = False
-model.compile(loss=SkyUtils.customloss, optimizer='adam', metrics=['accuracy'])
+
+
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
 training_generator = dataGenerator.DataGenerator(image_IDs, labels)
 
 
-epochs = 100
+epochs = 1
 acc = []
 loss = []
 for i in range(epochs):
-    history = model.fit_generator(generator = training_generator, use_multiprocessing = False, workers = 4)
+    history = model.fit_generator(generator = training_generator, use_multiprocessing = False, workers = 16)
     acc.append(history.history['acc'])
     loss.append(history.history['loss'])
     model.save('C:/Users/ander/OneDrive/Desktop/DL/CV2_final_project/model_saves/first_model_{}.h5'.format(i))
@@ -77,8 +79,12 @@ canvas = cv2.resize(canvas, (224,224))
 output = SkyUtils.drawRectsFromPred(prediction[0],canvas)
 cv2.imshow('output', output); cv2.waitKey()
 
+img = cv2.imread('D:/VOCtrainval_11-May-2012/VOCdevkit/VOC2012/JPEGImages/2007_000032.jpg')
+img = cv2.resize(img, (224,224))
+output = SkyUtils.overlayPredsByColor(prediction[0], img)
+cv2.imshow('predictions by color',output);cv2.waitKey()
+
 model.save('C:/Users/ander/OneDrive/Desktop/DL/CV2_final_project/model_saves/first_model.h5')
 model.save_weights('C:/Users/ander/OneDrive/Desktop/DL/CV2_final_project/model_saves/first_model_weights.h5')
 
-from time import time
 
